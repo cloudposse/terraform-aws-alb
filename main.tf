407635c8fa8d1b703b8bebb236b01ee09b5667b0
Federico Márquez Córdova <fedemzcor@MacBook-Air-de-Federico.local>
2019-09-29 14:25:26 -0500

    name_prefix lint fix

--- main.tf
+++ main.tf
@@ -90,7 +90,6 @@ module "default_target_group_label" {
 resource "aws_lb_target_group" "default" {
 
   name_prefix          = "albtg"
-  #name                 = "${module.default_target_group_label.id}"
   port                 = "80"
   protocol             = "HTTP"
   vpc_id               = "${var.vpc_id}"
