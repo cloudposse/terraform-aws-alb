e417542e8ef788b6873ea5944be922bd2124b723
Federico MC <desarrollo.ti@cleber.com>
2020-01-16 13:09:41 -0600

    ok

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "default_label" {
-  source     = "git::https://github.com/cloudposse/terraform-terraform-label.git?ref=tags/0.1.3"
+  source     = "git::https://github.com/cloudposse/terraform-terraform-label.git?ref=tags/0.16.0"
   attributes = var.attributes
   delimiter  = var.delimiter
   name       = var.name
