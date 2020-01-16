392171e47f2e8ec7188a0b8f4beff691142dea2d
Federico MC <desarrollo.ti@cleber.com>
2020-01-16 13:11:50 -0600

    ok

--- main.tf
+++ main.tf
@@ -1,11 +1,12 @@
 module "default_label" {
-  source     = "git::https://github.com/cloudposse/terraform-terraform-label.git?ref=tags/0.16.0"
-  attributes = var.attributes
-  delimiter  = var.delimiter
-  name       = var.name
-  namespace  = var.namespace
-  stage      = var.stage
-  tags       = var.tags
+  source      = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.16.0"
+  attributes  = var.attributes
+  delimiter   = var.delimiter
+  name        = var.name
+  namespace   = var.namespace
+  stage       = var.stage
+  environment = var.environment
+  tags        = var.tags
 }
 
 resource "aws_security_group" "default" {
