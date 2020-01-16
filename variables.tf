1b62ffceb0fca3b6b235947620f085e44757d2eb
Federico MC <desarrollo.ti@cleber.com>
2020-01-16 12:58:40 -0600

    ok

--- variables.tf
+++ variables.tf
@@ -1,248 +1,223 @@
 variable "namespace" {
-  type        = string
-  description = "Namespace (e.g. `eg` or `cp`)"
-  default     = ""
+  type        = "string"
+  description = "Namespace, which could be your organization name, e.g. `cp` or `cloudposse`"
 }
 
 variable "stage" {
-  type        = string
-  description = "Stage (e.g. `prod`, `dev`, `staging`)"
-  default     = ""
-}
-
-variable "environment" {
-  type        = string
-  description = "Environment, e.g. 'prod', 'staging', 'dev', 'pre-prod', 'UAT'"
-  default     = ""
+  type        = "string"
+  description = "Stage, e.g. `prod`, `staging`, `dev`, or `test`"
 }
 
 variable "name" {
-  type        = string
-  description = "Name of the application"
+  type        = "string"
+  description = "Solution name, e.g. `app`"
 }
 
 variable "delimiter" {
-  type        = string
+  type        = "string"
   default     = "-"
-  description = "Delimiter between `namespace`, `stage`, `name` and `attributes`"
+  description = "Delimiter to be used between `namespace`, `name`, `stage` and `attributes`"
 }
 
 variable "attributes" {
-  type        = list(string)
-  description = "Additional attributes (_e.g._ \"1\")"
+  type        = "list"
   default     = []
+  description = "Additional attributes, e.g. `1`"
 }
 
 variable "tags" {
-  type        = map(string)
-  description = "Additional tags (_e.g._ { BusinessUnit : ABC })"
+  type        = "map"
   default     = {}
+  description = "Additional tags (e.g. `map(`BusinessUnit`,`XYZ`)"
 }
 
 variable "vpc_id" {
-  type        = string
+  type        = "string"
   description = "VPC ID to associate with ALB"
 }
 
 variable "subnet_ids" {
-  type        = list(string)
+  type        = "list"
   description = "A list of subnet IDs to associate with ALB"
 }
 
 variable "security_group_ids" {
-  type        = list(string)
+  type        = "list"
   default     = []
   description = "A list of additional security group IDs to allow access to ALB"
 }
 
 variable "internal" {
-  type        = bool
-  default     = false
+  type        = "string"
+  default     = "false"
   description = "A boolean flag to determine whether the ALB should be internal"
 }
 
 variable "http_port" {
-  type        = number
-  default     = 80
+  type        = "string"
+  default     = "80"
   description = "The port for the HTTP listener"
 }
 
 variable "http_enabled" {
-  type        = bool
-  default     = true
+  type        = "string"
+  default     = "true"
   description = "A boolean flag to enable/disable HTTP listener"
 }
 
-variable "http_redirect" {
-  type        = bool
-  default     = false
-  description = "A boolean flag to enable/disable HTTP redirect to HTTPS"
-}
-
 variable "http_ingress_cidr_blocks" {
-  type        = list(string)
+  type        = "list"
   default     = ["0.0.0.0/0"]
   description = "List of CIDR blocks to allow in HTTP security group"
 }
 
 variable "http_ingress_prefix_list_ids" {
-  type        = list(string)
+  type        = "list"
   default     = []
   description = "List of prefix list IDs for allowing access to HTTP ingress security group"
 }
 
 variable "certificate_arn" {
-  type        = string
+  type        = "string"
   default     = ""
   description = "The ARN of the default SSL certificate for HTTPS listener"
 }
 
 variable "https_port" {
-  type        = number
-  default     = 443
+  type        = "string"
+  default     = "443"
   description = "The port for the HTTPS listener"
 }
 
 variable "https_enabled" {
-  type        = bool
-  default     = false
+  type        = "string"
+  default     = "false"
   description = "A boolean flag to enable/disable HTTPS listener"
 }
 
 variable "https_ingress_cidr_blocks" {
-  type        = list(string)
+  type        = "list"
   default     = ["0.0.0.0/0"]
   description = "List of CIDR blocks to allow in HTTPS security group"
 }
 
 variable "https_ingress_prefix_list_ids" {
-  type        = list(string)
+  type        = "list"
   default     = []
   description = "List of prefix list IDs for allowing access to HTTPS ingress security group"
 }
 
 variable "https_ssl_policy" {
-  type        = string
-  description = "The name of the SSL Policy for the listener"
+  description = "The name of the SSL Policy for the listener."
   default     = "ELBSecurityPolicy-2015-05"
 }
 
 variable "access_logs_prefix" {
-  type        = string
+  type        = "string"
   default     = ""
-  description = "The S3 log bucket prefix"
+  description = "The S3 bucket prefix"
 }
 
 variable "access_logs_enabled" {
-  type        = bool
-  default     = true
+  type        = "string"
+  default     = "true"
   description = "A boolean flag to enable/disable access_logs"
 }
 
 variable "access_logs_region" {
-  type        = string
+  type        = "string"
   default     = "us-east-1"
   description = "The region for the access_logs S3 bucket"
 }
 
 variable "cross_zone_load_balancing_enabled" {
-  type        = bool
-  default     = true
+  type        = "string"
+  default     = "true"
   description = "A boolean flag to enable/disable cross zone load balancing"
 }
 
 variable "http2_enabled" {
-  type        = bool
-  default     = true
+  type        = "string"
+  default     = "true"
   description = "A boolean flag to enable/disable HTTP/2"
 }
 
 variable "idle_timeout" {
-  type        = number
-  default     = 60
+  type        = "string"
+  default     = "60"
   description = "The time in seconds that the connection is allowed to be idle"
 }
 
 variable "ip_address_type" {
-  type        = string
+  type        = "string"
   default     = "ipv4"
   description = "The type of IP addresses used by the subnets for your load balancer. The possible values are `ipv4` and `dualstack`."
 }
 
 variable "deletion_protection_enabled" {
-  type        = bool
-  default     = false
+  type        = "string"
+  default     = "false"
   description = "A boolean flag to enable/disable deletion protection for ALB"
 }
 
 variable "deregistration_delay" {
-  type        = number
-  default     = 15
+  type        = "string"
+  default     = "15"
   description = "The amount of time to wait in seconds before changing the state of a deregistering target to unused"
 }
 
 variable "health_check_path" {
-  type        = string
+  type        = "string"
   default     = "/"
   description = "The destination for the health check request"
 }
 
 variable "health_check_timeout" {
-  type        = number
-  default     = 10
+  type        = "string"
+  default     = "10"
   description = "The amount of time to wait in seconds before failing a health check request"
 }
 
 variable "health_check_healthy_threshold" {
-  type        = number
-  default     = 2
+  type        = "string"
+  default     = "2"
   description = "The number of consecutive health checks successes required before considering an unhealthy target healthy"
 }
 
 variable "health_check_unhealthy_threshold" {
-  type        = number
-  default     = 2
+  type        = "string"
+  default     = "2"
   description = "The number of consecutive health check failures required before considering the target unhealthy"
 }
 
 variable "health_check_interval" {
-  type        = number
-  default     = 15
+  type        = "string"
+  default     = "15"
   description = "The duration in seconds in between health checks"
 }
 
 variable "health_check_matcher" {
-  type        = string
+  type        = "string"
   default     = "200-399"
   description = "The HTTP response codes to indicate a healthy check"
 }
 
 variable "alb_access_logs_s3_bucket_force_destroy" {
-  type        = bool
-  default     = false
   description = "A boolean that indicates all objects should be deleted from the ALB access logs S3 bucket so that the bucket can be destroyed without error"
+  default     = false
 }
 
 variable "target_group_port" {
-  type        = number
-  default     = 80
   description = "The port for the default target group"
+  default     = "80"
 }
 
-variable "target_group_name" {
-  type        = string
-  default     = ""
-  description = "The name for the default target group, uses a module label name if left empty"
+variable "target_type" {
+  default = "ip"
 }
 
-variable "target_group_target_type" {
-  type        = string
-  default     = "ip"
-  description = "The type (`instance`, `ip` or `lambda`) of targets that can be registered with the target group"
-}
+variable "access_logs_bucket" {
 
-variable "target_group_additional_tags" {
-  type        = map(string)
-  default     = {}
-  description = "The additional tags to apply to the target group"
+  default = "logs"
+  
 }
