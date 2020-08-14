cb0967476fbf301f9fbeace73d113c2a1c8f769b
Anton Shakh <shakh@quintagroup.org>
2020-08-14 16:17:32 +0300

    bump aws-lb-s3-bucket version

--- main.tf
+++ main.tf
@@ -48,7 +48,7 @@ resource "aws_security_group_rule" "https_ingress" {
 }
 
 module "access_logs" {
-  source                             = "git::https://github.com/cloudposse/terraform-aws-lb-s3-bucket.git?ref=tags/0.4.0"
+  source                             = "git::https://github.com/cloudposse/terraform-aws-lb-s3-bucket.git?ref=tags/0.5.0"
   enabled                            = var.access_logs_enabled
   name                               = var.name
   namespace                          = var.namespace
