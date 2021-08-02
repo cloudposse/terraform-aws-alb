697db302514c6e7f12d59ad0be360110d78f3e3f
nitrocode <nitrocode@users.noreply.github.com>
2021-08-02 17:10:03 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -57,7 +57,6 @@ module "access_logs" {
 module "default_load_balancer_label" {
   source          = "cloudposse/label/null"
   version         = "0.24.1"
-  attributes      = concat(module.this.attributes, ["default"])
   id_length_limit = var.load_balancer_name_max_length
   context         = module.this.context
 }
