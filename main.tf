7128c6196babd90b3d3831c8a4c6cc02e64a8b60
nitrocode <nitrocode@users.noreply.github.com>
2021-08-17 21:53:58 -0400

    Add `module.this.enabled` check to listener's `count` (#97)
    * Add module.this.enabled to listener
    
    * set tg count 1 only if fixed response is null
    
    * Update main.tf

--- main.tf
+++ main.tf
@@ -98,7 +98,7 @@ module "default_target_group_label" {
 }
 
 resource "aws_lb_target_group" "default" {
-  count                = module.this.enabled ? 1 : 0
+  count                = module.this.enabled && var.listener_http_fixed_response == null && var.listener_https_fixed_response == null ? 1 : 0
   name                 = var.target_group_name == "" ? module.default_target_group_label.id : substr(var.target_group_name, 0, var.target_group_name_max_length)
   port                 = var.target_group_port
   protocol             = var.target_group_protocol
@@ -139,7 +139,7 @@ resource "aws_lb_target_group" "default" {
 resource "aws_lb_listener" "http_forward" {
   #bridgecrew:skip=BC_AWS_GENERAL_43 - Skipping Ensure that load balancer is using TLS 1.2.
   #bridgecrew:skip=BC_AWS_NETWORKING_29 - Skipping Ensure ALB Protocol is HTTPS
-  count             = var.http_enabled && var.http_redirect != true ? 1 : 0
+  count             = module.this.enabled && var.http_enabled && var.http_redirect != true ? 1 : 0
   load_balancer_arn = join("", aws_lb.default.*.arn)
   port              = var.http_port
   protocol          = "HTTP"
