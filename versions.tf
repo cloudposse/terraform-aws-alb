7ce3b57981c3037b8913b4da7058038fbf799173
Kevin Mahoney <716806+kevcube@users.noreply.github.com>
2022-03-02 11:58:33 -0500

    lock AWS provider to version 3.x

--- versions.tf
+++ versions.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 2.54"
+      version = "~> 3.0"
     }
     local = {
       source  = "hashicorp/local"
